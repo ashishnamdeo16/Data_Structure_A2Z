ce7e986be9879115f4ef34917b66670c6729b7ae
ashishnamdeo <ashishnamdev14@gmail.com>
2025-07-25 00:42:02 -0700

    day2 Done

---
+++ patterns/pattern3.java
@@ -0,0 +1,63 @@
+class pattern3{
+    public static void main(String[] args) {
+        //Pattern8
+//        for(int i=4;i>=0;i--){
+//            for(int k=4-i;k>0;k--){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<(2*i)+1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+
+        //Pattern7
+//        for(int i=0;i<5;i++){
+//            for(int j=5;j>i;j--){
+//                System.out.print(" ");
+//            }
+//            for(int k=(2*i)+1;k>0;k--){
+//                System.out.print("*");
+//            }
+//                System.out.println("");
+//        }
+
+
+        //Pattern6
+//        for(int i=5;i>0;i--){
+//            for(int j=1;j<=i;j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+
+
+        //Pattern5
+//        for(int i=5;i>0;i--){
+//            for(int j=i;j>0;j--){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+
+        //Pattern4
+//        for(int i=0;i<5;i++){
+//            for(int j=0;j<=i;j++){
+//                System.out.print(i+1);
+//            }
+//            System.out.println();
+//        }
+
+
+
+        //Pattern3
+//        for(int i=0;i<=5;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+    }
+}
