9bd25958dd3050e42c86894e1017de8e8115e8b3
ashishnamdeo <ashishnamdev14@gmail.com>
2025-07-26 04:11:10 -0700

    day3

--- patterns/pattern4.java
+++ patterns/pattern4.java
@@ -1,23 +1,75 @@
 class pattern4{
     public static void main(String[] args) {
-    for(int i=0;i<5;i++){
-        for(int j=5;j>i;j--){
-            System.out.print(" ");
-        }
-        for(int k=(2*i)+1;k>0;k--){
-            System.out.print("*");
-        }
-        System.out.println("");
-    }
-        for(int l=5-2;l>=0;l--){
-            for(int m=5;m>l+1;m--){
-                System.out.print("*");
-            }
-            for(int n=0;n<2*l+1;n++){
-                System.out.print(" ");
-            }
-            System.out.println("");
-        }
+        //Pattern 13
+//        int num =1;
+//        for(int i=0;i<5;i++){
+//            for(int j=0;j<=i;j++){
+//                System.out.print(num++);
+//            }
+//            System.out.println();
+//        }
+
+        //Pattern 12
+//        for(int i=0;i<4;i++){
+//            for(int j=0;j<i+1;j++){
+//                System.out.print(j+1);
+//            }
+//            for(int k=6-(2*i);k>0;k--){
+//                System.out.print(" ");
+//            }
+//            for(int l=i+1;l>0;l--){
+//                System.out.print(l);
+//            }
+//            System.out.println();
+//        }
+
+        //Pattern 11
+//       for(int i=0;i<5;i++){
+//           for(int j=0;j<=i;j++){
+//               if((j+i) % 2 == 0 ){
+//                   System.out.print("1");
+//               }else{
+//                   System.out.print("0");
+//               }
+//           }
+//           System.out.println();
+//       }
+
+     //Pattern 10
+//     for(int i=0;i<5;i++){
+//         for(int j=i+1;j>0;j--){
+//             System.out.print("*");
+//         }
+//         System.out.println("");
+//     }
+//        for(int i=0;i<4;i++){
+//            for(int j=4-i;j>0;j--){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+
+
+
+    //Pattern9
+//    for(int i=0;i<5;i++){
+//        for(int j=5;j>i;j--){
+//            System.out.print(" ");
+//        }
+//        for(int k=(2*i)+1;k>0;k--){
+//            System.out.print("*");
+//        }
+//        System.out.println("");
+//    }
+//        for(int l=5;l>0;l--){
+//            for(int m=6-l;m>0;m--){
+//                System.out.print(" ");
+//            }
+//            for(int n=0;n<2*l-1;n++){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
     }
 
 }
