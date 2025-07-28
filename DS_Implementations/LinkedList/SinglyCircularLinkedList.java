36630ecdb7062a2fa5d2a00d7bbb62b7df4b0d87
ashishnamdeo <ashishnamdev14@gmail.com>
2025-07-28 16:50:38 -0700

    Implemented Circular Linked List

---
+++ DS_Implementations/LinkedList/SinglyCircularLinkedList.java
@@ -0,0 +1,31 @@
+public class SinglyCircularLinkedList{
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        head.next = second;
+        second.next = third;
+        third.next = head;
+
+        //Pointer
+        Node curr = head;
+
+
+        //Printing the Linked List
+        do{
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }while(curr != head);
+
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
